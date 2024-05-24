6edd1ce32cbb324733d35c46ba9b75874db85617
SVR Kalluru <raghuk.vit@gmail.com>
2024-05-24 06:46:48 +0530

    Image

--- run.sh
+++ run.sh
@@ -5,12 +5,12 @@ if [ -z "${DB_HOST}" ]; then
   exit 1
 fi
 
-if [ -z "${DB_USER}" ]; then
+if [ -z "${RDS_USER}" ]; then
   echo DB_USER is missing
   exit 1
 fi
 
-if [ -z "${DB_PASS}" ]; then
+if [ -z "${RDS_PASS}" ]; then
   echo DB_PASS is missing
   exit 1
 fi
@@ -28,7 +28,7 @@ fi
 git clone https://github.com/raghudevopsb78/${PROJECT_NAME}-${COMPONENT}
 cd ${PROJECT_NAME}-$COMPONENT
 
-mysql -h${DB_HOST} -u${DB_USER} -p${DB_PASS} <schema/${COMPONENT}.sql
+mysql -h${DB_HOST} -u${RDS_USER} -p${RDS_PASS} <schema/${COMPONENT}.sql
 
 
 
